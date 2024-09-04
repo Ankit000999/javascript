169894f217ff2ce6ef0144f6c73d786166634ef1
Ankit000999 <151570051+Ankit000999@users.noreply.github.com>
2024-09-04 07:10:11 +0530

    printing Odd numbers between 0 to 100

---
+++ seventh.js
@@ -0,0 +1,10 @@
+//using for loop
+//printing all the Odd numbers between  0 to 100
+
+for ( let i=0;i<=100 ; i++  ) 
+    { 
+        if(i%2!==0){
+        console.log("i= ",i );
+        }
+      
+    }
