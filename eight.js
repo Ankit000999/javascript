c53d2201971aade31d37354a842e0fc71c917ac5
Ankit000999 <151570051+Ankit000999@users.noreply.github.com>
2024-09-04 07:14:45 +0530

    learning to use prompt (through a game  )

---
+++ eight.js
@@ -0,0 +1,11 @@
+//using the prompt.
+//a game on loop
+
+let gameNum =35;
+let userNum= prompt ("Guess the game Number :");
+while(userNum != gameNum){
+   userNum = prompt ("you entered the wrong number. Try again.");
+}
+console.log ("Congratulations, For Guessing The Right Number .")
+
+
