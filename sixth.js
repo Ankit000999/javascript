5dce405d53e8d6095447e4dca870572de9249989
Ankit000999 <151570051+Ankit000999@users.noreply.github.com>
2024-09-04 07:00:06 +0530

    printing even number between 0 to 100

---
+++ sixth.js
@@ -0,0 +1,10 @@
+//using for loop.
+// Q-Print all the Even numbers between  0 to 100 ?
+
+for ( let i=0;i<=100 ; i++  ) 
+    { 
+        if(i%2===0){
+        console.log("i= ",i );
+        }
+      
+    }
