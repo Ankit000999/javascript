ea3125d0ee5db240811fc46b3c3d3a196d406979
Ankit000999 <151570051+Ankit000999@users.noreply.github.com>
2024-09-04 22:52:43 +0530

    pratice Question 1

---
+++ nine.js
@@ -0,0 +1,7 @@
+
+//using prompt.
+//Question :-Create a username in JS in such a way that it uses user input's length?
+
+let fullName= prompt ("Enter your Fullname without spaces :");
+let userName="@"+fullName +fullName.length;
+console.log(userName);
