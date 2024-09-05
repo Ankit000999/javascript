c142dfd960edfe190571cc5701eacb2162c9a62e
Ankit000999 <151570051+Ankit000999@users.noreply.github.com>
2024-09-05 22:45:36 +0530

    using splice

---
+++ thirteen.js
@@ -0,0 +1,6 @@
+//Q-Remove the "Uber" & add "Ola" in its place.
+//using splice to remove uber and add ola in its place.
+
+let companies=["Bloomberg","Mircosoft","Uber","Google","IBM","Netflix"];
+companies.splice(2,1,"Ola");
+console.log(companies);
