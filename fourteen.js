2d80df94887a3863321bdc77e44bb0bb90026d39
Ankit000999 <151570051+Ankit000999@users.noreply.github.com>
2024-09-05 22:50:58 +0530

    pratice Question  2

---
+++ fourteen.js
@@ -0,0 +1,8 @@
+//Q- Add amazon at the end.
+// we can also use splice to solve .
+// using push to add amazon in end.
+
+let companies=["Bloomberg","Mircosoft","Uber","Google","IBM","Netflix"];
+//companies.splice(6,0,"Amazon");
+companies.push("Amazon");
+console.log(companies);
