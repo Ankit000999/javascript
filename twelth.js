36045d964b1cb5a074b5e7dc9a21fa4bc1341338
Ankit000999 <151570051+Ankit000999@users.noreply.github.com>
2024-09-05 22:42:56 +0530

    using shift

---
+++ twelth.js
@@ -0,0 +1,6 @@
+//Q- Delete the first term in given array from start.
+//using shift to delete the first item in an array.
+ 
+let companies=["Bloomberg","Mircosoft","Uber","Google","IBM","Netflix"];
+companies.shift();
+console.log(companies);
