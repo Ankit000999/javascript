20cebba19730f4abfbfc891b52a24a41f6d4e893
Ankit000999 <151570051+Ankit000999@users.noreply.github.com>
2024-09-07 01:18:44 +0530

    using filter

---
+++ eighteenth.js
@@ -0,0 +1,12 @@
+
+
+
+//using filter in array  and finding the even number using filter function .
+
+let arr=[111,121,131,141,150];
+
+let newArr = arr.filter((val) => {
+    return val % 2 === 0;
+}); 
+console.log(newArr);
+
