ce5c2cdecd3ab39c5cc8d6e50a013d34c9886efc
Ankit000999 <151570051+Ankit000999@users.noreply.github.com>
2024-09-08 17:38:27 +0530

    using function

---
+++ tweenty.js
@@ -0,0 +1,10 @@
+
+
+//Learning How to Create a function .
+
+function myFunction ()
+{
+    console.log("JavaScript is a Good Programming Language.");
+    console.log("I am learning Javascript.");
+}
+myFunction();
