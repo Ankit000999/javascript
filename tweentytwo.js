13c075c14ac61d311556145cadd902c0bcc378f1
Ankit000999 <151570051+Ankit000999@users.noreply.github.com>
2024-09-08 17:41:34 +0530

    using Arrow function

---
+++ tweentytwo.js
@@ -0,0 +1,28 @@
+
+//using sum function and Arrow function
+
+
+//sum function 
+function sum(a,b){
+    return a+b;
+}
+
+//Mordern JavaScript
+//Arrow function 
+ const arrowSum = (a,b)=>{
+    console.log(a+b);
+};
+
+
+//using multiplication function and Arrow function
+
+//multiplication function 
+function mul(a,b){
+    return a*b;
+}
+
+//Mordern JavaScript
+//Arrow function
+const arrowMul=(a,b) =>{
+    console.log(a*b);  
+};
